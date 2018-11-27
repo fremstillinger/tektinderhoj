a934cabd6714bc6e4ba78c49fe0a570e5cdf8f75
Martin Luckmann <martin@fremstillinger.dk>
2018-11-27 11:12:57 +0100

    updt

---
+++ tristarMorningstarLogger.js
@@ -0,0 +1,60 @@
+var ModbusRTU = require("modbus-serial");
+var client = new ModbusRTU();
+const fs = require('fs');
+const WebSocket = require('ws');
+const apiCalls = require('./apiCalls');
+var cron = require('cron');
+
+// reload readingtypes every hour
+new cron.CronJob({
+    cronTime: '* 00 * * * *',
+    onTick: updateReadingTypes,
+    start: true
+});
+
+
+var configData = JSON.parse(fs.readFileSync(__dirname + '/config.json', 'utf8'));
+
+// open connection to a serial port
+client.connectRTUBuffered(configData.tristarUSBadress, {
+    baudRate: 9600
+});
+
+client.setID(1);
+
+var v_scale = 96.667 * 2 ** (-15);
+var ampScale = 66.667 * 2 ** (-15);
+
+function doReading() {
+
+    client.readHoldingRegisters(0, 20, function(err, data) {
+     
+        if (data != undefined) {
+
+            for (var i = 0; i < readingTypes.length; i++) {
+                var rt = readingTypes[i];
+                if (rt.deviceType == "tristar") {
+                    var readingIndex = rt.readingData * 1 - 1;
+                    var value = data.data[readingIndex];
+                  
+                    eval(rt.readingConversion);
+            
+                    apiCalls.logData(rt.readingTypeID, configData.tristartDeviceID, value);
+                }
+            }
+
+        }
+    });
+}
+
+var readingTypes = [];
+
+function updateReadingTypes() {
+    apiCalls.getReadingTypes(function(d) {
+        readingTypes = d;
+    })
+};
+
+updateReadingTypes();
+
+setInterval(doReading, 2000);
