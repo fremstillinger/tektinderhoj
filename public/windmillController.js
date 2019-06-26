4baf50b32eed9f2472adcf1ebd8976265db5c08c
Martin Luckmann <martin@fremstillinger.dk>
2019-06-26 13:55:07 +0200

    updtled

--- public/windmillController.js
+++ public/windmillController.js
@@ -154,8 +154,8 @@ app.controller('windmillCtrl', ['$scope', '$routeParams', '$route', '$http', '$t
 				$scope.batteryLevel = 1;
 				$scope.batteryCharging = false;
 				 	$scope.updateLights();
-				 	$http.get('http://localhost:8282/startRun/0/75/255/255/0/20');
-				 	$http.get('http://localhost:8282/setColor/76/500/80/40/0');
+				 	$http.get('http://localhost:8282/startRun/0/70/255/255/0/20');
+				 	$http.get('http://localhost:8282/setColor/71/500/80/40/0');
 			}	
 		}
 		else{
@@ -164,8 +164,8 @@ app.controller('windmillCtrl', ['$scope', '$routeParams', '$route', '$http', '$t
 			if($scope.batteryLevel <= 0){
 				$scope.batteryLevel = 0;
 				 $scope.updateLights();
-				 $http.get('http://localhost:8282/stopRun/0/75/255/255/0/50');
-				 $http.get('http://localhost:8282/setColor/76/500/0/0/0');
+				 $http.get('http://localhost:8282/stopRun/0/70/255/255/0/50');
+				 $http.get('http://localhost:8282/setColor/71/500/0/0/0');
 				$scope.batteryCharging = true;
 			}
 		}
