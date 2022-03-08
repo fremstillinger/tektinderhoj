bc8890ed3cdce6b3ac01e72c802bbdcd3a47cbd2
Martin Luckmann <martin@fremstillinger.dk>
2022-03-08 15:15:29 +0100

    updt

--- public/windmillController.js
+++ public/windmillController.js
@@ -56,8 +56,8 @@ app.controller('windmillCtrl', ['$scope', '$routeParams', '$route', '$http', '$t
     }
 
 
-    $scope.requestWindSpeedFromDatabase();
-    setInterval($scope.requestWindSpeedFromDatabase, 1000 * 15);
+   // $scope.requestWindSpeedFromDatabase();
+    setInterval($scope.requestWindSpeedFromDatabase, 1000 * 30);
 
 
 
@@ -236,7 +236,7 @@ app.controller('windmillCtrl', ['$scope', '$routeParams', '$route', '$http', '$t
                 break;
 
             case 73:
-                //$window.location.reload();
+                $window.location.reload();
                 break;
 
 
