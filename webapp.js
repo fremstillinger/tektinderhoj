a94d63b7fb064d2d5ca4989192029acaf2aad493
Martin Luckmann <martin@fremstillinger.dk>
2022-03-17 15:41:28 +0100

    updt

--- webapp.js
+++ webapp.js
@@ -1,7 +1,6 @@
 const mysql = require('mysql');
 const fs = require('fs');
 const express = require('express');
-const cors = require('cors'); 
 const bodyParser = require("body-parser");
 const app = express();
 //app.use(cors());
